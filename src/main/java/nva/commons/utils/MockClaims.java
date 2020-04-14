08b376560aa040fdcc21fb251eddb55ae915f4a9
Orestis Gkorgkas <orestis.gorgas@gmail.com>
2020-04-14 09:38:52 +0200

    fix: mock-claims javadoc update

--- src/main/java/nva/commons/utils/MockClaims.java
+++ src/main/java/nva/commons/utils/MockClaims.java
@@ -4,6 +4,34 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 
+/**
+ * Mocks the user claims. For usage when we are running a lambda locally through SAM. In the current version, the method
+ * has to be called by the locally run handler, in the {@code handleRequest) method or the {@code processInput method}.
+ * <p>
+ * <p>
+ * Example for the processInputMethod.
+ *
+ * <pre>
+ *  public class MyHandler extends ApiGatewayHandler<RequestBody,String>{
+ *
+ *     &#64;Override
+ *     protected String processInput(RequestBody input, RequestInfo requestInfo, Context context)
+ *         throws ApiGatewayException {
+ *         ... do the processing here...
+ *     }
+ *
+ *     &#64;Override
+ *     protected final String processInput(RequestBody input, String apiGatewayInputString, Context context)
+ *     throws ApiGatewayException {
+ *         JsonNode event = jsonParser.readTree(apiGatewayInputString);
+ *         JsonNode eventWithClaims = MockClaims.apiGatewayEvent(event,jsonParser);
+ *         String eventWithClaimsStr = jsonParser.writeValueAsString(eventWithClaims);
+ *         RequestInfo requestInfo = inputParser.getRequestInfo(eventWithClaimsStr);
+ *         return processInput(input, requestInfo, context);
+ *     }
+ * }
+ * </pre>
+ */
 public final class MockClaims {
 
     public static final String CUSTOM_FEIDE_ID = "custom:feideId";
@@ -18,7 +46,9 @@ public final class MockClaims {
     }
 
     /**
-     * Mocks the user claims. For usage when we are running a lambda locally through SAM.
+     * Mocks the user claims. For usage when we are running a lambda locally through SAM. In the current version, the
+     * method has to be called by the locally run handler, in the  {@code handleRequest} method or the
+     * {@code processInput} method.
      *
      * @param event      the ApiGateway event
      * @param jsonParser a jsonParser
