e6dfdf94f7d1ca7b99e433fcf5faaeccbb368c31
Orestis Gkorgkas <orestis.gorgas@gmail.com>
2020-04-14 08:29:16 +0200

    fix: env and json tests

--- src/test/java/nva/commons/utils/EnvironmentTest.java
+++ src/test/java/nva/commons/utils/EnvironmentTest.java
@@ -3,6 +3,7 @@ package nva.commons.utils;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.Optional;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class EnvironmentTest {
@@ -13,14 +14,16 @@ public class EnvironmentTest {
     private Environment environment = new Environment();
 
     @Test
+    @DisplayName("readEnvOpt returns an empty optional when env variable does not exist")
     public void readEnvOptShouldReturnAnEmptyOptionalForNonExistingEnvVariable() {
         Optional<String> value = environment.readEnvOpt(NON_EXISTING_ENV_VARIABLE);
         assertTrue(value.isEmpty());
     }
 
     @Test
-    public void readEnvOptShouldReturnAnOptionalForNonExistingEnvVariable() {
-        Optional<String> value = environment.readEnvOpt(NON_EXISTING_ENV_VARIABLE);
-        assertTrue(value.isEmpty());
+    @DisplayName("readEnvOpt returns an non empty optional when env variable  exists")
+    public void readEnvOptShouldReturnAnNonEmptyOptionalForExistingEnvVariable() {
+        Optional<String> value = environment.readEnvOpt(EXISTING_ENV_VARIABLE);
+        assertTrue(value.isPresent());
     }
 }
