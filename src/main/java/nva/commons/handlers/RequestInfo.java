3f04092e8144cdaffdeddf97098a533f41d51d66
Orestis Gkorgkas <orestis.gorgas@gmail.com>
2020-04-22 10:54:48 +0200

    feat: RequestInfo has query parameters

--- src/main/java/nva/commons/handlers/RequestInfo.java
+++ src/main/java/nva/commons/handlers/RequestInfo.java
@@ -1,6 +1,7 @@
 package nva.commons.handlers;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.HashMap;
 import java.util.Map;
 import nva.commons.utils.JacocoGenerated;
@@ -11,7 +12,9 @@ public class RequestInfo {
     public static final String PROXY_TAG = "proxy";
     private Map<String, String> headers;
     private String path;
-    private Map<String,String> pathParameters;
+    private Map<String, String> pathParameters;
+    @JsonProperty("queryStringParameters")
+    private Map<String, String> queryParameters;
 
     public RequestInfo() {
         this.headers = new HashMap<>();
@@ -39,14 +42,23 @@ public class RequestInfo {
     }
 
     @JacocoGenerated
-    public Map<String,String> getPathParameters() {
+    public Map<String, String> getPathParameters() {
         return pathParameters;
     }
 
     @JacocoGenerated
-    public void setPathParameters(Map<String,String> pathParameters) {
+    public void setPathParameters(Map<String, String> pathParameters) {
         this.pathParameters = pathParameters;
     }
 
+    @JacocoGenerated
+    public Map<String, String> getQueryParameters() {
+        return queryParameters;
+    }
+
+    @JacocoGenerated
+    public void setQueryParameters(Map<String, String> queryParameters) {
+        this.queryParameters = queryParameters;
+    }
 }
 
