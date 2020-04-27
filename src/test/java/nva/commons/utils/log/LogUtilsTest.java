5d4dc55f4e183c05e065013b39349d223d2dcab1
Orestis Gkorgkas <orestis.gorgas@gmail.com>
2020-04-27 14:26:51 +0200

    NP-711: remove Environment from LogUtilsTest

--- src/test/java/nva/commons/utils/log/LogUtilsTest.java
+++ src/test/java/nva/commons/utils/log/LogUtilsTest.java
@@ -2,24 +2,10 @@ package nva.commons.utils.log;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.StringContains.containsString;
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
-import nva.commons.utils.Environment;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class LogUtilsTest {
-
-    public static final String ANY_VALUE = "ANY_VALUE";
-    private Environment environment;
-
-    @BeforeEach
-    public void setup() {
-        Environment environment = mock(Environment.class);
-        when(environment.readEnv(anyString())).thenReturn(ANY_VALUE);
-    }
 
     @Test
     public void getTestingAppenderReturnsAnAppenderWithTheLoggedMessages() {
