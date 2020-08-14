a4574244651ed771be66cc9fb1a731659b440135
Orestis Gkorgkas <orestis.gorgas@gmail.com>
2020-08-14 16:04:21 +0200

    fix: Jacoco ignore setters getters

--- src/main/java/nva/commons/handlers/authentication/Event.java
+++ src/main/java/nva/commons/handlers/authentication/Event.java
@@ -4,21 +4,25 @@ import com.fasterxml.jackson.annotation.JsonAnyGetter;
 import com.fasterxml.jackson.annotation.JsonAnySetter;
 import java.util.LinkedHashMap;
 import java.util.Map;
+import nva.commons.utils.JacocoGenerated;
 
 public class Event {
 
     @JsonAnySetter
     public Map<String, Object> properties;
 
+    @JacocoGenerated
     public Event() {
         this.properties = new LinkedHashMap<>();
     }
 
     @JsonAnyGetter
+    @JacocoGenerated
     public Map<String, Object> getProperties() {
         return properties;
     }
 
+    @JacocoGenerated
     public void setProperties(Map<String, Object> properties) {
         this.properties = properties;
     }
