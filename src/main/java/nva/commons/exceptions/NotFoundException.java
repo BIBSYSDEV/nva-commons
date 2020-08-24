8b2a5d909944d8aa1c1a1201387b546e3214e35c
Orestis Gkorgkas <orestis.gorgas@gmail.com>
2020-08-24 21:34:12 +0200

    feat: NotFoundException

---
+++ src/main/java/nva/commons/exceptions/NotFoundException.java
@@ -0,0 +1,23 @@
+package nva.commons.exceptions;
+
+import org.apache.http.HttpStatus;
+
+public class NotFoundException extends ApiGatewayException {
+
+    public NotFoundException(String message) {
+        super(message);
+    }
+
+    public NotFoundException(Exception exception) {
+        super(exception);
+    }
+
+    public NotFoundException(Exception exception, String message) {
+        super(exception, message);
+    }
+
+    @Override
+    protected Integer statusCode() {
+        return HttpStatus.SC_NOT_FOUND;
+    }
+}
---
+++ src/test/java/nva/commons/exceptions/NotFoundExceptionTest.java
@@ -0,0 +1,50 @@
+package nva.commons.exceptions;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsEqual.equalTo;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintWriter;
+import org.junit.jupiter.api.Test;
+
+public class NotFoundExceptionTest {
+
+    public static final String SOME_MESSAGE = "Some message";
+    private static final String CAUSE_MESSAGE = "Cause message";
+
+    @Test
+    public void notFoundExceptionShowsSuppliedMessage() {
+        NotFoundException exception = new NotFoundException(SOME_MESSAGE);
+        assertThat(exception.getMessage(), containsString(SOME_MESSAGE));
+    }
+
+    @Test
+    public void notFoundExceptionShowsSuppliedCause() {
+        Exception cause = new Exception(CAUSE_MESSAGE);
+        NotFoundException exception = new NotFoundException(cause);
+        ByteArrayOutputStream stackTraceOutput = getStackTraceMessage(exception);
+        assertThat(exception.getCause(), is(equalTo(cause)));
+        assertThat(stackTraceOutput.toString(), containsString(CAUSE_MESSAGE));
+    }
+
+    @Test
+    public void notFoundExceptionShowsSuppliedMessageAndSuppliecCause() {
+        Exception cause = new Exception(CAUSE_MESSAGE);
+        NotFoundException exception = new NotFoundException(cause, SOME_MESSAGE);
+        ByteArrayOutputStream stackTraceOutput = getStackTraceMessage(exception);
+        assertThat(exception.getCause(), is(equalTo(cause)));
+        String stackTraceOutputString = stackTraceOutput.toString();
+        assertThat(stackTraceOutputString, containsString(CAUSE_MESSAGE));
+        assertThat(stackTraceOutputString, containsString(SOME_MESSAGE));
+    }
+
+    private ByteArrayOutputStream getStackTraceMessage(NotFoundException exception) {
+        ByteArrayOutputStream stackTraceOutput = new ByteArrayOutputStream();
+        PrintWriter writer = new PrintWriter(stackTraceOutput, true);
+        exception.printStackTrace(writer);
+        writer.close();
+        return stackTraceOutput;
+    }
+}
