4cc18f7d05e848d523471ee893cba9e9d45ded25
Orestis Gkorgkas <orestis.gkorgkas@unit.no>
2020-12-29 21:13:43 +0100

    break-into-modules: doi package

---
+++ doi/src/test/java/nva/commons/doi/DoiConverterTest.java
@@ -0,0 +1,74 @@
+package nva.commons.doi;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.hamcrest.core.StringContains.containsString;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import java.net.URI;
+
+import nva.commons.commons.TestAppender;
+import nva.commons.commons.logutils.LogUtils;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class DoiConverterTest {
+
+    private static final String DOI = "10.1000/182";
+    private static final String EXPECTED = "https://doi.org/" + DOI;
+
+    DoiConverter doiConverterImpl = new DoiConverter();
+
+    @DisplayName("toUri returns a URI if input is a valid DOI URI")
+    @Test
+    public void toUriReturnsAUriIfInputIsAValidDoiUri() {
+        String input = "https://doi.org/" + DOI;
+        URI actual = doiConverterImpl.toUri(input);
+        assertThat(actual.toString(), is(equalTo(input)));
+    }
+
+    @DisplayName("toUri returns a URI when input is a doi identifier")
+    @Test
+    public void toUriReturnsAUriIfInputIsADoiIdentifier() {
+        URI actual = doiConverterImpl.toUri(DOI);
+        assertThat(actual.toString(), is(equalTo(EXPECTED)));
+    }
+
+    @DisplayName("toUri returns an HTTPS URI when input is an HTTP URI")
+    @Test
+    public void toUriReturnsAUriIfInputIsAnHttpDoiUri() {
+        String input = "http://doi.org/" + DOI;
+        URI actual = doiConverterImpl.toUri(input);
+        assertThat(actual.toString(), is(equalTo(EXPECTED)));
+    }
+
+    @Test
+    @DisplayName("toUri throws Exception when input is not a valid URI")
+    public void toUriThrowsAnExceptionWhenInputIsNotValidUri() {
+        TestAppender appender = LogUtils.getTestingAppender(DoiConverter.class);
+        String input = "http://somethingelse.org/" + DOI;
+        IllegalStateException exception = assertThrows(IllegalStateException.class,
+            () -> doiConverterImpl.toUri(input));
+        assertThat(exception.getMessage(), containsString(input));
+        assertThat(appender.getMessages(), containsString(input));
+    }
+
+    @Test
+    @DisplayName("toURI returns a URI when input is a DOI string with DOI prefix")
+    public void toUriReturnsAUriIfUriWhenInputIsDoiStringWithDoiPrefix() {
+        String input = "doi:" + DOI;
+        URI actual = doiConverterImpl.toUri(input);
+        assertThat(actual.toString(), is(equalTo(EXPECTED)));
+    }
+
+    @Test
+    @DisplayName("toURI throws Exception when input is an invalid DOI string")
+    public void toUriThrowsExceptionWhenInputIsAnInvalidDoiString() {
+        TestAppender appender = LogUtils.getTestingAppender(DoiConverter.class);
+        String input = "213456";
+        IllegalStateException exception = assertThrows(IllegalStateException.class,
+            () -> doiConverterImpl.toUri(input));
+        assertThat(exception.getMessage(), containsString(input));
+        assertThat(appender.getMessages(), containsString(input));
+    }
+}
---
+++ doi/src/test/java/nva/commons/doi/DoiValidatorTest.java
@@ -0,0 +1,67 @@
+package nva.commons.doi;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsEqual.equalTo;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URL;
+import org.junit.jupiter.api.Test;
+
+public class DoiValidatorTest {
+
+    private static final String DOI = "10.1000/182";
+    private static final String DOI_PREFIX = "doi:";
+    private static final String HTTPS_DOI = "https://doi.org/";
+    private static final String HTTP_DOI = "http://doi.org/";
+    private static final String HTTPS_DX_DOI = "https://dx.doi.org/";
+    private static final String INVALID_URL = "https://something.com";
+    private static final String INVALID_PREFIX = "odi:";
+
+    @Test
+    public void validateReturnsTrueForDoiStringWithoutDoiPrefix() {
+        assertThat(DoiValidator.validate(DOI), is((equalTo(true))));
+    }
+
+    @Test
+    public void validateReturnsTrueForDoiStringWitDoiPrefix() {
+        String input = DOI_PREFIX + DOI;
+        assertThat(DoiValidator.validate(input), is((equalTo(true))));
+    }
+
+    @Test
+    public void validateReturnsTrueForHttpsDoiWitDoiUri() {
+        String input = HTTPS_DOI + DOI;
+        assertThat(DoiValidator.validate(input), is((equalTo(true))));
+    }
+
+    @Test
+    public void validateReturnsTrueForHttpDoiWitDoiUri() {
+        String input = HTTP_DOI + DOI;
+        assertThat(DoiValidator.validate(input), is((equalTo(true))));
+    }
+
+    @Test
+    public void validateReturnsTrueForHttpsDxDoiWitDoiUri() {
+        String input = HTTPS_DX_DOI + DOI;
+        assertThat(DoiValidator.validate(input), is((equalTo(true))));
+    }
+
+    @Test
+    public void validateReturnsFalseForInvalidDoiUri() {
+        String input = INVALID_URL + DOI;
+        assertThat(DoiValidator.validate(input), is((equalTo(false))));
+    }
+
+    @Test
+    public void validateReturnsFalseForInvalidDoiPrefix() {
+        String input = INVALID_PREFIX + DOI;
+        assertThat(DoiValidator.validate(input), is((equalTo(false))));
+    }
+
+    @Test
+    public void validateReturnsTrueForValidURL() throws MalformedURLException {
+        URL input = URI.create(HTTP_DOI + DOI).toURL();
+        assertThat(DoiValidator.validate(input), is((equalTo(true))));
+    }
+}
