c2abcf80a766b94ff5becaf9c3b73a3ecab8f0f1
Orestis Gkorgkas <orestis.gkorgkas@unit.no>
2021-01-29 15:14:56 +0100

    NP-1679: Content-Type header

--- apigateway/src/main/java/nva/commons/apigateway/HttpHeaders.java
+++ apigateway/src/main/java/nva/commons/apigateway/HttpHeaders.java
@@ -4,6 +4,7 @@ public final class HttpHeaders {
 
     public static final String AUTHORIZATION = "Authorization";
     public static final String LOCATION = "Location";
+    public static final String CONTENT_TYPE = "Content-Type";
 
     private HttpHeaders() {
 
