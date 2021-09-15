a37962eba7e73b337b7b6111101023c16e47d75a
Jan Bjørndalen <jbj@aztek.no>
2021-09-15 12:07:41 +0200

    Added fina modifier

--- apigateway/src/main/java/nva/commons/apigateway/ContentTypes.java
+++ apigateway/src/main/java/nva/commons/apigateway/ContentTypes.java
@@ -5,7 +5,7 @@ import java.util.Locale;
 public final class ContentTypes {
 
     public static final String APPLICATION_XML = "application/xml".toLowerCase(Locale.ROOT);
-    public static String WILDCARD = "*/*".toLowerCase(Locale.ROOT);
+    public static final String WILDCARD = "*/*".toLowerCase(Locale.ROOT);
     public static final String APPLICATION_JSON = "application/json".toLowerCase(Locale.ROOT);
     public static final String APPLICATION_JSON_LD = "application/ld+json".toLowerCase(Locale.ROOT);
 
