7d373bdce038c6dc4d4150573ce063d27e72d84e
Orestis Gkorgkas <orestis.gkorgkas@unit.no>
2021-10-07 14:36:49 +0200

    restore JsonSerializable functionality

--- core/src/main/java/nva/commons/core/JsonSerializable.java
+++ core/src/main/java/nva/commons/core/JsonSerializable.java
@@ -9,7 +9,7 @@ public interface JsonSerializable {
      */
     default String toJsonString() {
         try {
-            return JsonUtils.dtoObjectMapper.writeValueAsString(this);
+            return JsonUtils.singleLineObjectMapper.writeValueAsString(this);
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
