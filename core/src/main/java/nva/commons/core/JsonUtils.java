b13fea7d972489f70c666dfe5e1699460451e0f3
Orestis Gkorgkas <orestis.gkorgkas@unit.no>
2021-10-15 11:29:32 +0200

    instruct intellij to not inspect convert2Diamond in the custom serializing module

--- core/src/main/java/nva/commons/core/JsonUtils.java
+++ core/src/main/java/nva/commons/core/JsonUtils.java
@@ -56,6 +56,7 @@ public final class JsonUtils {
     private static SimpleModule emptyStringAsNullModule() {
         SimpleModule module = new SimpleModule();
         //Do not optimize the line under. It can fail due to a Oracle JDK bug.
+        //noinspection Convert2Diamond
         module.addDeserializer(String.class, new StdDeserializer<String>(String.class) {
 
             @Override
