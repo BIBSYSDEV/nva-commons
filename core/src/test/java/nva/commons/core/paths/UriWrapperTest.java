ac4a1271b2ae7167e9706cf25ab694acf612d7db
Orestis Gkorgkas <orestis.gkorgkas@unit.no>
2021-11-08 15:12:18 +0100

    UriWrapper supports query parameters

--- core/src/test/java/nva/commons/core/paths/UriWrapperTest.java
+++ core/src/test/java/nva/commons/core/paths/UriWrapperTest.java
@@ -94,7 +94,19 @@ class UriWrapperTest {
     }
 
     @Test
-    public void shouldReturnUriWithQueryParametersWhenQueryParametersArePresent() {
+    public void shouldReturnUriWithQueryParametersWhenSingleQueryParameterIsPresent() {
+        URI expectedUri = URI.create("https://www.example.org/path1/path2?key1=value1");
+        URI uri = URI.create("https://www.example.org/");
+        URI actualUri = new UriWrapper(uri)
+            .addChild("path1")
+            .addQueryParameter("key1", "value1")
+            .addChild("path2")
+            .getUri();
+        assertThat(actualUri, is(equalTo(expectedUri)));
+    }
+
+    @Test
+    public void shouldReturnUriWithQueryParametersWhenManyQueryParametersArePresent() {
         URI expectedUri = URI.create("https://www.example.org/path1/path2?key1=value1&key2=value2");
         URI uri = URI.create("https://www.example.org/");
         URI actualUri = new UriWrapper(uri)
