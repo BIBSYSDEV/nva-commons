0e19d8bf96aa3500554251b35e71827ade26f5fe
Jan Bjørndalen <jbj@aztek.no>
2022-02-23 10:36:16 +0100

    Added datacite xml content type

--- apigateway/src/main/java/nva/commons/apigateway/MediaTypes.java
+++ apigateway/src/main/java/nva/commons/apigateway/MediaTypes.java
@@ -6,6 +6,7 @@ public final class MediaTypes {
 
     public static final MediaType APPLICATION_JSON_LD = MediaType.create("application", "ld+json");
     public static final MediaType APPLICATION_PROBLEM_JSON = MediaType.create("application", "problem+json");
+    public static final MediaType APPLICATION_DATACITE_XML = MediaType.create("application", "vnd.datacite.datacite+xml");
 
     private MediaTypes() {
     }
