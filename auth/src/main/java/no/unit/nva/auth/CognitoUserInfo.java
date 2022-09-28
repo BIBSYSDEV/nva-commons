c1fa881e59dc40eb73eaa84d1695fcd82847766d
truhacevkir <kir.truhacev@sikt.no>
2022-09-28 14:12:01 +0200

    feide-id-requestInfo, extracting FeideId from claims implemented

--- auth/src/main/java/no/unit/nva/auth/CognitoUserInfo.java
+++ auth/src/main/java/no/unit/nva/auth/CognitoUserInfo.java
@@ -12,7 +12,7 @@ public class CognitoUserInfo {
 
     public static final String ELEMENTS_DELIMITER = ",";
     public static final String EMPTY_STRING = "";
-    public static final String FEIDE_ID_CLAIM = "custom:feideId";
+    public static final String PERSON_FEIDE_ID_CLAIM = "custom:feideId";
     public static final String SELECTED_CUSTOMER_CLAIM = "custom:customerId";
     public static final String ACCESS_RIGHTS_CLAIM = "custom:accessRights";
     public static final String USER_NAME_CLAIM = "custom:nvaUsername";
@@ -25,7 +25,7 @@ public class CognitoUserInfo {
     public static final String PERSON_AFFILIATION = "custom:personAffiliation";
     public static final String ALLOWED_CUSTOMERS = "custom:allowedCustomers";
     public static final String COGNITO_USER_NAME = "username";
-    @JsonProperty(FEIDE_ID_CLAIM)
+    @JsonProperty(PERSON_FEIDE_ID_CLAIM)
     private String feideId;
     @JsonProperty(SELECTED_CUSTOMER_CLAIM)
     private URI currentCustomer;
@@ -169,17 +169,17 @@ public class CognitoUserInfo {
         if (this == o) {
             return true;
         }
-        if (!(o instanceof CognitoUserInfo)) {
+        if (!(o instanceof CognitoUserInfo that)) {
             return false;
         }
-        CognitoUserInfo that = (CognitoUserInfo) o;
         return Objects.equals(getFeideId(), that.getFeideId())
                && Objects.equals(getCurrentCustomer(), that.getCurrentCustomer())
                && Objects.equals(getAccessRights(), that.getAccessRights())
                && Objects.equals(getUserName(), that.getUserName())
                && Objects.equals(getTopOrgCristinId(), that.getTopOrgCristinId())
                && Objects.equals(getPersonCristinId(), that.getPersonCristinId())
-               && Objects.equals(getPersonNin(), that.getPersonNin());
+               && Objects.equals(getPersonNin(), that.getPersonNin())
+               && Objects.equals(getFeideId(), that.getFeideId());
     }
 
     public static final class Builder {
