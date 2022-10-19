e3543d84f495879411a29742948d981b4035a559
Orestis Gkorgkas <orestis.gkorgkas@unit.no>
2022-10-19 15:59:16 +0200

    extract bucket name containing the event

--- eventhandlers/src/test/java/no/unit/nva/events/models/EventReferenceTest.java
+++ eventhandlers/src/test/java/no/unit/nva/events/models/EventReferenceTest.java
@@ -7,6 +7,7 @@ import static org.hamcrest.core.Is.is;
 import static org.hamcrest.core.IsEqual.equalTo;
 import static org.hamcrest.core.StringContains.containsString;
 import java.net.URI;
+import java.time.Instant;
 import org.junit.jupiter.api.Test;
 
 class EventReferenceTest {
@@ -20,7 +21,7 @@ class EventReferenceTest {
         assertThat(json, containsString(expectedTopic));
         assertThat(json, containsString(expectedUri.toString()));
     }
-
+    
     @Test
     void shouldDeSerializeFromJsonObjectWithoutInformationLoss() {
         String expectedTopic = randomString();
@@ -28,5 +29,12 @@ class EventReferenceTest {
         EventReference eventReference = new EventReference(expectedTopic, expectedUri);
         EventReference deserializedEventReference = EventReference.fromJson(eventReference.toJsonString());
         assertThat(deserializedEventReference, is(equalTo(eventReference)));
+    }
+    
+    @Test
+    void shouldProvideBucketNameContainingTheEvent() {
+        var s3uri = URI.create("s3://expected-bucket/path/to/file");
+        var eventReference = new EventReference(randomString(), randomString(), s3uri, Instant.now());
+        assertThat(eventReference.extractBucketName(), is(equalTo("expected-bucket")));
     }
 }
