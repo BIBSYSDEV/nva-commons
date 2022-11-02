f45186ba97b3861c48202a6b7feffcf2420f4df3
brinxmat <789709+brinxmat@users.noreply.github.com>
2022-11-02 22:06:11 +0100

    Extract constant to avoid PMD warning

--- apigateway/src/main/java/nva/commons/apigateway/MediaTypes.java
+++ apigateway/src/main/java/nva/commons/apigateway/MediaTypes.java
@@ -4,13 +4,14 @@ import com.google.common.net.MediaType;
 
 public final class MediaTypes {
 
-    public static final MediaType APPLICATION_JSON_LD = MediaType.create("application", "ld+json");
+    public static final String APPLICATION = "application";
+    public static final MediaType APPLICATION_JSON_LD = MediaType.create(APPLICATION, "ld+json");
     public static final MediaType APPLICATION_PROBLEM_JSON =
-        MediaType.create("application", "problem+json");
+        MediaType.create(APPLICATION, "problem+json");
     public static final MediaType APPLICATION_DATACITE_XML =
-        MediaType.create("application", "vnd.datacite.datacite+xml");
+        MediaType.create(APPLICATION, "vnd.datacite.datacite+xml");
     public static final MediaType SCHEMA_ORG =
-        MediaType.create("application", "vnd.schemaorg.ld+json");
+        MediaType.create(APPLICATION, "vnd.schemaorg.ld+json");
 
     private MediaTypes() {
     }
