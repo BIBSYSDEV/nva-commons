ec2aa1bf3a9ba99f271daf4dd66d9db636d9918e
Sondre Vestad <sondre.vestad@tietoevry.com>
2023-03-22 13:28:28 +0100

    added identity service client

---
+++ clients/src/test/java/no/unit/nva/clients/BackendClientCredentialsTest.java
@@ -0,0 +1,31 @@
+package no.unit.nva.clients;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import no.unit.nva.commons.json.JsonUtils;
+import org.junit.jupiter.api.Test;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+
+class BackendClientCredentialsTest {
+
+    @Test
+    public void gettersShouldReturnCorrectValues() {
+        var credentials = new BackendClientCredentials("id", "secret");
+
+        assertThat(credentials.getId(), is(equalTo("id")));
+        assertThat(credentials.getSecret(), is(equalTo("secret")));
+    }
+
+    @Test
+    public void toStringShouldCorrectlyRepresentObjectAsJson() throws JsonProcessingException {
+        var originalCredentials = new BackendClientCredentials("id", "secret");
+        var str = originalCredentials.toString();
+        var parsedCredentials = JsonUtils.dtoObjectMapper.readValue(str,
+                                                                    BackendClientCredentials.class);
+
+        assertThat(parsedCredentials.getId(), is(equalTo(originalCredentials.getId())));
+        assertThat(parsedCredentials.getSecret(), is(equalTo(originalCredentials.getSecret())));
+    }
+
+}
