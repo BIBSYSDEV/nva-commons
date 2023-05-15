2d7decc6278c53d85fef1638ada3231461ba8a1d
Orestis Gkorgkas <orestis.gorgas@gmail.com>
2023-05-15 14:18:45 +0200

    firehose-client

---
+++ nvatestutils/src/main/java/no/unit/nva/stubs/FakeFirehoseClient.java
@@ -0,0 +1,51 @@
+package no.unit.nva.stubs;
+
+import java.util.ArrayList;
+import java.util.List;
+import nva.commons.core.JacocoGenerated;
+import software.amazon.awssdk.services.firehose.FirehoseClient;
+import software.amazon.awssdk.services.firehose.model.PutRecordBatchRequest;
+import software.amazon.awssdk.services.firehose.model.PutRecordBatchResponse;
+import software.amazon.awssdk.services.firehose.model.PutRecordRequest;
+import software.amazon.awssdk.services.firehose.model.PutRecordResponse;
+import software.amazon.awssdk.services.firehose.model.Record;
+
+/**
+ *  A Kinesis Firehose collects the records that are pushed to it and stores them in an S3 bucket.
+ *  The important feature is that it collects multiple records in ZIP files and organizes the ZIP files by timestamp:
+ *  s3://some-bucket/some-folder/2023/05/08/18/some-prefix-2023-05-08-18-01-17-lfksdjflskd-srdfsdef-sefsdf-sdfsd
+ *  -sgdfsdfsdfs.gz
+ *
+ */
+public class FakeFirehoseClient implements FirehoseClient {
+
+    private final List<Record> records = new ArrayList<>();
+
+    @Override
+    public PutRecordResponse putRecord(PutRecordRequest putRecordRequest) {
+        records.add(putRecordRequest.record());
+        return PutRecordResponse.builder().build();
+    }
+
+    @Override
+    public PutRecordBatchResponse putRecordBatch(PutRecordBatchRequest putRecordBatchRequest) {
+        records.addAll(putRecordBatchRequest.records());
+        return PutRecordBatchResponse.builder().build();
+    }
+
+    @JacocoGenerated
+    @Override
+    public String serviceName() {
+        return "FakeFirehoseClient";
+    }
+
+    @Override
+    @JacocoGenerated
+    public void close() {
+        //NO-OP
+    }
+
+    public List<Record> getRecords() {
+        return this.records;
+    }
+}
---
+++ nvatestutils/src/test/java/no/unit/nva/stubs/FakeFirehoseClientTest.java
@@ -0,0 +1,55 @@
+package no.unit.nva.stubs;
+
+import static no.unit.nva.testutils.RandomDataGenerator.randomString;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.collection.IsIterableContainingInOrder.contains;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import nva.commons.core.SingletonCollector;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import software.amazon.awssdk.core.SdkBytes;
+import software.amazon.awssdk.services.firehose.model.PutRecordBatchRequest;
+import software.amazon.awssdk.services.firehose.model.PutRecordRequest;
+import software.amazon.awssdk.services.firehose.model.Record;
+
+public class FakeFirehoseClientTest {
+
+    private FakeFirehoseClient client;
+
+    @BeforeEach
+    public void init() {
+        this.client = new FakeFirehoseClient();
+    }
+
+    @Test
+    void shouldReturnTheRecordPushedToFirehoseAsIs() {
+        var expectedRecord = randomRecord();
+        var request = PutRecordRequest.builder().record(expectedRecord).build();
+        client.putRecord(request);
+        var actualRecord = client.getRecords().stream().collect(SingletonCollector.collect());
+        assertThat(actualRecord, is(equalTo(expectedRecord)));
+    }
+
+    @Test
+    void shouldReturnAllRecordsWhenPushingABatchOfRecords() {
+        var expectedRecords = List.of(randomRecord(), randomRecord());
+
+        PutRecordBatchRequest request = PutRecordBatchRequest.builder()
+                                            .records(expectedRecords)
+                                            .build();
+        client.putRecordBatch(request);
+        var actualRecords = client.getRecords();
+        assertThat(actualRecords, contains(expectedRecords.toArray(Record[]::new)));
+    }
+
+    private static Record randomRecord() {
+        return Record.builder().data(randomContent()).build();
+    }
+
+    private static SdkBytes randomContent() {
+        return SdkBytes.fromString(randomString(), StandardCharsets.UTF_8);
+    }
+}
