50c58cbcf12c42d20c71ff0bf1ad197473c697fa
Orestis Gkorgkas <orestis.gorgas@gmail.com>
2023-06-05 12:56:44 +0200

    FakeFirehoseClient supplies pushedContent

--- nvatestutils/src/main/java/no/unit/nva/stubs/FakeFirehoseClient.java
+++ nvatestutils/src/main/java/no/unit/nva/stubs/FakeFirehoseClient.java
@@ -1,7 +1,10 @@
 package no.unit.nva.stubs;
 
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Stream;
 import nva.commons.core.JacocoGenerated;
 import software.amazon.awssdk.services.firehose.FirehoseClient;
 import software.amazon.awssdk.services.firehose.model.PutRecordBatchRequest;
@@ -11,11 +14,10 @@ import software.amazon.awssdk.services.firehose.model.PutRecordResponse;
 import software.amazon.awssdk.services.firehose.model.Record;
 
 /**
- *  A Kinesis Firehose collects the records that are pushed to it and stores them in an S3 bucket.
- *  The important feature is that it collects multiple records in ZIP files and organizes the ZIP files by timestamp:
- *  s3://some-bucket/some-folder/2023/05/08/18/some-prefix-2023-05-08-18-01-17-lfksdjflskd-srdfsdef-sefsdf-sdfsd
- *  -sgdfsdfsdfs.gz
- *
+ * A Kinesis Firehose collects the records that are pushed to it and stores them in an S3 bucket. The important feature
+ * is that it collects multiple records in ZIP files and organizes the ZIP files by timestamp:
+ * s3://some-bucket/some-folder/2023/05/08/18/some-prefix-2023-05-08-18-01-17-lfksdjflskd-srdfsdef-sefsdf-sdfsd
+ * -sgdfsdfsdfs.gz
  */
 public class FakeFirehoseClient implements FirehoseClient {
 
@@ -47,5 +49,15 @@ public class FakeFirehoseClient implements FirehoseClient {
 
     public List<Record> getRecords() {
         return this.records;
+    }
+
+    public Stream<String> extractPushedContent() {
+        return this.getRecords().stream()
+                   .map(Record::data)
+                   .map(data -> data.asString(StandardCharsets.UTF_8));
+    }
+
+    public <T> Stream<T> extractPushedContent(Function<String, T> parser) {
+        return extractPushedContent().map(parser::apply);
     }
 }
--- nvatestutils/src/test/java/no/unit/nva/stubs/FakeFirehoseClientTest.java
+++ nvatestutils/src/test/java/no/unit/nva/stubs/FakeFirehoseClientTest.java
@@ -1,13 +1,21 @@
 package no.unit.nva.stubs;
 
+import static no.unit.nva.testutils.RandomDataGenerator.randomJson;
 import static no.unit.nva.testutils.RandomDataGenerator.randomString;
+import static nva.commons.core.attempt.Try.attempt;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsInAnyOrder;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.collection.IsIterableContainingInOrder.contains;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import java.nio.charset.StandardCharsets;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import nva.commons.core.SingletonCollector;
+import nva.commons.core.attempt.Try;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import software.amazon.awssdk.core.SdkBytes;
@@ -18,6 +26,7 @@ import software.amazon.awssdk.services.firehose.model.Record;
 public class FakeFirehoseClientTest {
 
     private FakeFirehoseClient client;
+    private ObjectMapper OBJECT_MAPPER = new ObjectMapper();
 
     @BeforeEach
     public void init() {
@@ -38,18 +47,48 @@ public class FakeFirehoseClientTest {
         var expectedRecords = List.of(randomRecord(), randomRecord());
 
         var request = PutRecordBatchRequest.builder()
-                                            .records(expectedRecords)
-                                            .build();
+                          .records(expectedRecords)
+                          .build();
         client.putRecordBatch(request);
         var actualRecords = client.getRecords();
         assertThat(actualRecords, contains(expectedRecords.toArray(Record[]::new)));
     }
 
-    private static Record randomRecord() {
-        return Record.builder().data(randomContent()).build();
+    @Test
+    void shouldProvideParsedEmittedContent() {
+        var expectedContent = randomJsons();
+        var records = crateRecords(expectedContent);
+
+        var request = PutRecordBatchRequest.builder().records(records).build();
+        client.putRecordBatch(request);
+        var actualContent = client.extractPushedContent(this::parseString).collect(Collectors.toList());
+
+        assertThat(actualContent, contains(expectedContent.toArray(JsonNode[]::new)));
     }
 
-    private static SdkBytes randomContent() {
-        return SdkBytes.fromString(randomString(), StandardCharsets.UTF_8);
+    private List<Record> crateRecords(List<JsonNode> expectedContent) {
+        return expectedContent.stream()
+                   .map(attempt(OBJECT_MAPPER::writeValueAsString))
+                   .flatMap(Try::stream)
+                   .map(FakeFirehoseClientTest::toRecord)
+                   .collect(Collectors.toList());
+    }
+
+    private List<JsonNode> randomJsons() {
+        return Stream.of(randomJson(), randomJson())
+                   .map(this::parseString)
+                   .collect(Collectors.toList());
+    }
+
+    private static Record toRecord(String content) {
+        return Record.builder().data(SdkBytes.fromString(content, StandardCharsets.UTF_8)).build();
+    }
+
+    private static Record randomRecord() {
+        return toRecord(randomString());
+    }
+
+    private JsonNode parseString(String jsonString) {
+        return attempt(() -> OBJECT_MAPPER.readTree(jsonString)).orElseThrow();
     }
 }
