4971728459a02bb09a878d7e406da1bbb80aa80f
Haakon Heiberg <haakon.heiberg@sikt.no>
2023-10-19 08:09:45 +0200

    Add support for setting isBase64Encoded in ApiGatewayHandler response

--- apigateway/src/main/java/nva/commons/apigateway/GatewayResponse.java
+++ apigateway/src/main/java/nva/commons/apigateway/GatewayResponse.java
@@ -7,7 +7,6 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
@@ -25,6 +24,7 @@ public class GatewayResponse<T> implements Serializable {
     private final String body;
     private final Map<String, String> headers;
     private final int statusCode;
+    private final boolean isBase64Encoded;
 
     /**
      * Constructor for JSON deserializing.
@@ -37,10 +37,12 @@ public class GatewayResponse<T> implements Serializable {
     public GatewayResponse(
         @JsonProperty("body") final String body,
         @JsonProperty("headers") final Map<String, String> headers,
-        @JsonProperty("statusCode") final int statusCode) {
+        @JsonProperty("statusCode") final int statusCode,
+        @JsonProperty("isBase64Encoded") final boolean isBase64Encoded) {
         this.body = body;
         this.headers = headers;
         this.statusCode = statusCode;
+        this.isBase64Encoded = isBase64Encoded;
     }
 
     /**
@@ -52,12 +54,29 @@ public class GatewayResponse<T> implements Serializable {
      * @param objectMapper desired object mapper
      * @throws GatewayResponseSerializingException when serializing fails
      */
+    @JacocoGenerated
     public GatewayResponse(T body, Map<String, String> headers, int statusCode, ObjectMapper objectMapper)
         throws GatewayResponseSerializingException {
+        this(body, headers, statusCode, false, objectMapper);
+    }
+
+    /**
+     * Constructor for GatewayResponse.
+     *
+     * @param body            body of response
+     * @param headers         http headers for response
+     * @param statusCode      status code for response
+     * @param isBase64Encoded status code for response
+     * @param objectMapper    desired object mapper
+     * @throws GatewayResponseSerializingException when serializing fails
+     */
+    public GatewayResponse(T body, Map<String, String> headers, int statusCode, Boolean isBase64Encoded,
+                           ObjectMapper objectMapper) throws GatewayResponseSerializingException {
         try {
             this.statusCode = statusCode;
             this.body = body instanceof String ? (String) body : objectMapper.writeValueAsString(body);
             this.headers = Map.copyOf(headers);
+            this.isBase64Encoded = isBase64Encoded;
         } catch (JsonProcessingException e) {
             throw new GatewayResponseSerializingException(e);
         }
@@ -66,50 +85,53 @@ public class GatewayResponse<T> implements Serializable {
     /**
      * Builder for GatewayResponse.
      *
-     * @param outputStream       content of response
+     * @param outputStream content of response
      * @throws JsonProcessingException when serializing fails
      */
     public static <T> GatewayResponse<T> of(ByteArrayOutputStream outputStream) throws JsonProcessingException {
-        var typeReference =  new TypeReference<GatewayResponse<T>>() { };
+        var typeReference = new TypeReference<GatewayResponse<T>>() {
+        };
         var json = outputStream.toString(StandardCharsets.UTF_8);
-        return defaultRestObjectMapper.readValue(json,typeReference);
+        return defaultRestObjectMapper.readValue(json, typeReference);
     }
 
     /**
      * Builder for GatewayResponse.
      *
-     * @param inputStream       content of response
+     * @param inputStream content of response
      * @throws JsonProcessingException when serializing fails
      */
     public static <T> GatewayResponse<T> of(InputStream inputStream) throws IOException {
-        var typeReference =  new TypeReference<GatewayResponse<T>>() { };
-        return defaultRestObjectMapper.readValue(inputStream,typeReference);
+        var typeReference = new TypeReference<GatewayResponse<T>>() {
+        };
+        return defaultRestObjectMapper.readValue(inputStream, typeReference);
     }
 
     /**
      * Builder for GatewayResponse.
      *
-     * @param jsonString       content of response
+     * @param jsonString content of response
      * @throws JsonProcessingException when serializing fails
      */
     public static <T> GatewayResponse<T> of(String jsonString) throws JsonProcessingException {
-        var typeReference = new TypeReference<GatewayResponse<T>>() { };
-        return defaultRestObjectMapper.readValue(jsonString,typeReference);
+        var typeReference = new TypeReference<GatewayResponse<T>>() {
+        };
+        return defaultRestObjectMapper.readValue(jsonString, typeReference);
     }
-
 
     /**
      * Create GatewayResponse object from an output stream. Used when we call the method {@code handleRequest()} of a
      * Handler directly, and we want to read the output.
+     *
      * @param outputStream the outputStream updated by the lambda handler
-     * @param className the class of the returned object
+     * @param className    the class of the returned object
+     * @param <T>          The type of the interface or class returned
      * @return the GatewayResponse containing the output of the handler
-     * @param <T> The type of the interface or class returned
      * @throws JsonProcessingException when the OutputStream cannot be parsed to a JSON object.
      */
     public static <T> GatewayResponse<T> fromOutputStream(ByteArrayOutputStream outputStream, Class<T> className)
         throws JsonProcessingException {
-        String json = outputStream.toString(StandardCharsets.UTF_8);
+        var json = outputStream.toString(StandardCharsets.UTF_8);
         return fromString(json, className);
     }
 
@@ -117,7 +139,7 @@ public class GatewayResponse<T> implements Serializable {
     @JacocoGenerated
     public static <T> GatewayResponse<T> fromOutputStream(ByteArrayOutputStream outputStream)
         throws JsonProcessingException {
-        String json = outputStream.toString(StandardCharsets.UTF_8);
+        var json = outputStream.toString(StandardCharsets.UTF_8);
         return fromString(json);
     }
 
@@ -144,31 +166,6 @@ public class GatewayResponse<T> implements Serializable {
         return constructResponseWithJsonObjectBody(responseString);
     }
 
-    private static <T> boolean isString(Class<T> className) {
-        return className.getTypeName().equals(String.class.getTypeName());
-    }
-
-    private static <T> GatewayResponse<T> constructResponseWithJsonObjectBody(String responseString)
-        throws JsonProcessingException {
-        TypeReference<GatewayResponse<T>> typeref = new TypeReference<>() {
-        };
-        return defaultRestObjectMapper.readValue(responseString, typeref);
-    }
-
-    private static <T> GatewayResponse<T> constructGatewayResponseWithStringBody(String responseString)
-        throws JsonProcessingException {
-        JsonNode jsonNode = defaultRestObjectMapper.readTree(responseString);
-
-        String body = jsonNode.get("body").asText();
-        TypeReference<Map<String, String>> typeref = new TypeReference<>() {
-        };
-        Map<String, String> headers = defaultRestObjectMapper.convertValue(jsonNode.get("headers"), typeref);
-        int statusCode = jsonNode.get("statusCode").asInt();
-
-        return (GatewayResponse<T>)
-                   attempt(() -> new GatewayResponse(body, headers, statusCode, defaultRestObjectMapper)).orElseThrow();
-    }
-
     public String getBody() {
         return body;
     }
@@ -178,7 +175,6 @@ public class GatewayResponse<T> implements Serializable {
         return defaultRestObjectMapper.readValue(body, new TypeReference<>() {
         });
     }
-
 
     /**
      * Parses the JSON body to an object.
@@ -199,10 +195,14 @@ public class GatewayResponse<T> implements Serializable {
         return statusCode;
     }
 
+    public Boolean getIsBase64Encoded() {
+        return isBase64Encoded;
+    }
+
     @Override
     @JacocoGenerated
     public int hashCode() {
-        return Objects.hash(body, headers, statusCode);
+        return Objects.hash(body, headers, statusCode, isBase64Encoded);
     }
 
     @Override
@@ -214,9 +214,38 @@ public class GatewayResponse<T> implements Serializable {
         if (o == null || getClass() != o.getClass()) {
             return false;
         }
-        GatewayResponse<?> that = (GatewayResponse<?>) o;
+        var that = (GatewayResponse<?>) o;
         return statusCode == that.statusCode
                && Objects.equals(body, that.body)
-               && Objects.equals(headers, that.headers);
+               && Objects.equals(headers, that.headers)
+               && Objects.equals(isBase64Encoded, that.isBase64Encoded);
+    }
+
+    private static <T> boolean isString(Class<T> className) {
+        return className.getTypeName().equals(String.class.getTypeName());
+    }
+
+    private static <T> GatewayResponse<T> constructResponseWithJsonObjectBody(String responseString)
+        throws JsonProcessingException {
+        TypeReference<GatewayResponse<T>> typeref = new TypeReference<>() {
+        };
+        return defaultRestObjectMapper.readValue(responseString, typeref);
+    }
+
+    private static <T> GatewayResponse<T> constructGatewayResponseWithStringBody(String responseString)
+        throws JsonProcessingException {
+        var jsonNode = defaultRestObjectMapper.readTree(responseString);
+
+        var body = jsonNode.get("body").asText();
+        var typeRef = new TypeReference<Map<String, String>>() {
+        };
+        var headers = defaultRestObjectMapper.convertValue(jsonNode.get("headers"), typeRef);
+        var statusCode = jsonNode.get("statusCode").asInt();
+        var hasIsBase64EncodedNode = jsonNode.has("isBase64Encoded");
+        var isBase64Encoded = hasIsBase64EncodedNode && jsonNode.get("isBase64Encoded").asBoolean();
+
+        return (GatewayResponse<T>)
+                   attempt(() -> new GatewayResponse(body, headers, statusCode, isBase64Encoded,
+                                                     defaultRestObjectMapper)).orElseThrow();
     }
 }
