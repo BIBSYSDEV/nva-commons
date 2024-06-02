5a2a9e9ddf97e950c0e95d0497939c43fe5e63ff
monaullah <monaullah98@gmail.com>
2024-06-02 15:16:18 +0200

    Add ApiS3PresignerGatewayHandler

---
+++ apigateway/src/main/java/nva/commons/apigateway/ApiS3PresignerGatewayHandler.java
@@ -0,0 +1,66 @@
+package nva.commons.apigateway;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import java.net.HttpURLConnection;
+import java.time.Duration;
+import java.util.Map;
+import nva.commons.apigateway.exceptions.BadRequestException;
+import nva.commons.core.JacocoGenerated;
+import software.amazon.awssdk.services.s3.model.GetObjectRequest;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
+import software.amazon.awssdk.services.s3.presigner.model.GetObjectPresignRequest;
+import software.amazon.awssdk.services.s3.presigner.model.PresignedGetObjectRequest;
+
+public abstract class ApiS3PresignerGatewayHandler<I> extends ApiGatewayHandler<I, Void> {
+
+    public static final String LOCATION = "Location";
+    public static final Duration SIGN_DURATION = Duration.ofMinutes(60);
+    private final S3Presigner s3presigner;
+
+    public ApiS3PresignerGatewayHandler(Class<I> iclass, S3Presigner s3Presigner) {
+        super(iclass);
+        this.s3presigner = s3Presigner;
+    }
+
+    @Override
+    protected Void processInput(I input, RequestInfo requestInfo, Context context) throws BadRequestException {
+        var filename = context.getAwsRequestId();
+        var presign = presignS3Object(filename);
+        generateAndWriteDataToS3(filename, input);
+        setLocationHeader(presign.url().toString());
+        return null;
+    }
+
+    @Override
+    @JacocoGenerated
+    protected Integer getSuccessStatusCode(I input, Void output) {
+        return HttpURLConnection.HTTP_MOVED_TEMP;
+    }
+
+    protected abstract void generateAndWriteDataToS3(String filename, I input);
+
+    protected abstract String getBucketName();
+
+    private GetObjectPresignRequest createPresignRequest(String filename) {
+        return GetObjectPresignRequest.builder()
+                   .signatureDuration(SIGN_DURATION)
+                   .getObjectRequest(buildRequest(filename))
+                   .build();
+    }
+
+    private GetObjectRequest buildRequest(String filename) {
+        return GetObjectRequest.builder()
+                   .bucket(getBucketName())
+                   .key(filename)
+                   .build();
+    }
+
+    private void setLocationHeader(String uri) {
+        addAdditionalHeaders(() -> Map.of(LOCATION, uri));
+    }
+
+    private PresignedGetObjectRequest presignS3Object(String filename) {
+        var presignRequest = createPresignRequest(filename);
+        return s3presigner.presignGetObject(presignRequest);
+    }
+}
---
+++ apigateway/src/test/java/nva/commons/apigateway/ApiS3PresignerGatewayHandlerTest.java
@@ -0,0 +1,73 @@
+package nva.commons.apigateway;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import no.unit.nva.stubs.FakeContext;
+import nva.commons.core.ioutils.IoUtils;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import software.amazon.awssdk.http.HttpStatusCode;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
+import software.amazon.awssdk.services.s3.presigner.model.GetObjectPresignRequest;
+import software.amazon.awssdk.services.s3.presigner.model.PresignedGetObjectRequest;
+
+class ApiS3PresignerGatewayHandlerTest {
+
+    private S3Presigner s3Presigner;
+    private ByteArrayOutputStream output;
+    private InputStream input;
+    private ApiS3PresignerGatewayHandler<Void> handler;
+
+    @BeforeEach
+    void init() {
+        s3Presigner = mock(S3Presigner.class);
+        input = IoUtils.stringToStream("{}");
+        output = new ByteArrayOutputStream();
+        handler = createHandler();
+    }
+
+    @Test
+    void shouldSetLocationHeader() throws IOException {
+        var context = new FakeContext();
+        var expectedFilename = context.getAwsRequestId();
+
+        var presignedGetObjectRequest = mockPresignResponse(expectedFilename);
+        when(s3Presigner.presignGetObject(any(GetObjectPresignRequest.class))).thenReturn(presignedGetObjectRequest);
+
+        handler.handleRequest(input, output, context);
+        var response = GatewayResponse.fromOutputStream(output, Void.class);
+        assertThat(response.getHeaders().get("Location"), containsString(expectedFilename));
+        assertThat(response.getStatusCode(), is(equalTo(HttpStatusCode.MOVED_TEMPORARILY)));
+    }
+
+    private static PresignedGetObjectRequest mockPresignResponse(String filename) throws MalformedURLException {
+        var presignRequest = mock(PresignedGetObjectRequest.class);
+        var presignedUrl = "https://example.com/" + filename;
+        when(presignRequest.url()).thenReturn(new URL(presignedUrl));
+        return presignRequest;
+    }
+
+    private ApiS3PresignerGatewayHandler<Void> createHandler() {
+        return new ApiS3PresignerGatewayHandler<Void>(Void.class, s3Presigner) {
+
+            @Override
+            protected void generateAndWriteDataToS3(String filename, Void input) {
+            }
+
+            @Override
+            protected String getBucketName() {
+                return "someTestBucket";
+            }
+        };
+    }
+}
