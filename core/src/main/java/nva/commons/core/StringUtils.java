91754822d7118bc6a8447c9f5c5ab3eaa5524679
Kir Truhacev <107109909+truhacevkir@users.noreply.github.com>
2025-08-07 10:22:39 +0200

    Np 49498 adding remove xml tags method to stringutils (#564)
    Adding removeXmlTags method to StringUtils in commons. Method is
    extracted from StringUtils from nva-fetch-doi.
    After this pull request is merged, we can remove StringUtils from
    nva-fetch-doi and use common StringUtils for NVA instead.

--- core/src/main/java/nva/commons/core/StringUtils.java
+++ core/src/main/java/nva/commons/core/StringUtils.java
@@ -1,9 +1,27 @@
 package nva.commons.core;
 
 import static java.util.Objects.isNull;
+import java.io.IOException;
+import java.io.Reader;
+import java.io.StringReader;
+import java.nio.charset.StandardCharsets;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpression;
+import javax.xml.xpath.XPathExpressionException;
+import javax.xml.xpath.XPathFactory;
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
 
 public final class StringUtils {
 
+    private static final String BASIC_OUTER_XML_TAGS_TEMPLATE = "<naive>%s</naive>";
+    private static final String PATH_TO_TEXT = "//text()";
     public static final String DOUBLE_WHITESPACE = "\\s\\s";
     public static final String WHITESPACES = "\\s+";
     public static final String SPACE = " ";
@@ -86,5 +104,53 @@ public final class StringUtils {
      */
     public static boolean isNotBlank(String string) {
         return !isBlank(string);
+    }
+
+    public static String removeXmlTags(String input) {
+        String output = null;
+        DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newDefaultInstance();
+        try {
+            Document document = createXmlDocumentFromInput(input, documentBuilderFactory);
+            NodeList nodeList = getDocumentNodes(document);
+            output = textWithoutXmlTags(nodeList);
+        } catch (XPathExpressionException | ParserConfigurationException | IOException | SAXException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            if (isNull(output)) {
+                output = input;
+            }
+        }
+        return removeMultipleWhiteSpaces(output).trim();
+    }
+
+    private static String textWithoutXmlTags(NodeList nodeList) {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int counter = 0; counter < nodeList.getLength(); counter++) {
+            stringBuilder.append(EMPTY_STRING).append(nodeList.item(counter).getTextContent());
+        }
+        return stringBuilder.toString();
+    }
+
+    private static NodeList getDocumentNodes(Document document) throws XPathExpressionException {
+        XPathFactory xpathFactory = XPathFactory.newDefaultInstance();
+        XPath xpath = xpathFactory.newXPath();
+        XPathExpression expr = xpath.compile(PATH_TO_TEXT);
+        return (NodeList) expr.evaluate(document, XPathConstants.NODESET);
+    }
+
+    private static Document createXmlDocumentFromInput(String input, DocumentBuilderFactory documentBuilderFactory)
+        throws ParserConfigurationException, SAXException, IOException {
+        DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
+        try (Reader reader = new StringReader(wrapInXml(input))) {
+            InputSource inputSource = new InputSource(reader);
+            inputSource.setEncoding(StandardCharsets.UTF_8.name());
+            Document document = documentBuilder.parse(inputSource);
+            document.getDocumentElement().normalize();
+            return document;
+        }
+    }
+
+    private static String wrapInXml(String input) {
+        return String.format(BASIC_OUTER_XML_TAGS_TEMPLATE, input);
     }
 }
