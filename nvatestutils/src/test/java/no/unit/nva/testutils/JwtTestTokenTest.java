f3a45e7605bc47bd8559e6ff37ae4938aeafbfb6
Lars-Olav Vågene <42863501+LarsV123@users.noreply.github.com>
2026-06-01 12:27:33 +0200

    refactor: Replace java.util.Date with java.time.Instant

--- nvatestutils/src/test/java/no/unit/nva/testutils/JwtTestTokenTest.java
+++ nvatestutils/src/test/java/no/unit/nva/testutils/JwtTestTokenTest.java
@@ -3,7 +3,7 @@ package no.unit.nva.testutils;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
 import com.auth0.jwt.JWT;
-import java.util.Date;
+import java.time.Instant;
 import org.junit.jupiter.api.Test;
 
 class JwtTestTokenTest {
@@ -11,12 +11,12 @@ class JwtTestTokenTest {
     @Test
     void shouldCreateRandomJwt() {
         var actual = JwtTestToken.randomToken();
-        assertThat(JWT.decode(actual).getExpiresAt().after(new Date()), is(true));
+        assertThat(JWT.decode(actual).getExpiresAtAsInstant().isAfter(Instant.now()), is(true));
     }
 
     @Test
     void shouldCreateRandomExpiredJwt() {
         var actual = JwtTestToken.randomExpiredToken();
-        assertThat(JWT.decode(actual).getExpiresAt().before(new Date()), is(true));
+        assertThat(JWT.decode(actual).getExpiresAtAsInstant().isBefore(Instant.now()), is(true));
     }
 }
